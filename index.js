4911eefecea147f4b754cddab30164c3c8864d60
wigzen <61306958+wigzen@users.noreply.github.com>
2022-10-13 21:15:27 +0530

    FIX :score
    Now User can see the scores

--- index.js
+++ index.js
@@ -1,72 +1,64 @@
 import chalk from "chalk";
-import chalkAnimation from "chalk-animation";
-import figlet from "figlet";
+
 import { createSpinner } from "nanospinner";
 import ReadLine from "readline-sync";
 
 
 // Welcome
 function Welcome() {
-    // figlet("WELCOME", {
-    //     horizontalLayout: 'full',
-    //     width: 80,
-    //     whitespaceBreak: true
-    // },function (err, data) {
-    //   if (err) {
-    //     console.log("kaboom");
-    //     return;
-    //   }
-    // chalkAnimation.rainbow(data);
 
-      // setTimeout(() => {
-        let username = ReadLine.question('Tell me your name bro')
-        console.log(chalk.blue.bgGray("Hello")+" "+username)
-    // }, 1000);
-    
-    // });    
 
-// Play
+  // setTimeout(() => {
+  let username = ReadLine.question('Tell me your name bro')
+  console.log(chalk.blue.bgGray("Hello") + " " + username)
+  // }, 1000);
 
-let QnA = [
-    { 
-        'question':"What is My Date of Birth? format:'DDMMYYYY'",
-        'ans':"16022002"
+  // });    
+
+  // Play
+
+  let QnA = [
+    {
+      'question': "What is My Date of Birth? format:'DDMMYYYY'",
+      'ans': "16022002"
     },
-    { 
-        'question':"What is my favorite food ?",
-        'ans':"khichadi"
+    {
+      'question': "What is my favorite food ?",
+      'ans': "khichadi"
     },
-    { 
-        'question':"Which is my favorite place ?",
-        'ans':"Mumbai"
+    {
+      'question': "Which is my favorite place ?",
+      'ans': "Mumbai"
     },
-    { 
-        'question':"What is my Hobby ?",
-        'ans':"Reading Articles"
+    {
+      'question': "What is my Hobby ?",
+      'ans': "Reading Articles"
     },
-    { 
-        'question':"Do you know my current College",
-        'ans':"Thakur College"
+    {
+      'question': "Do you know my current College",
+      'ans': "Thakur College"
     }
-]
-let score =0
-    const spinner = createSpinner('Checking Answer')
-    QnA.forEach((val)=>{
-        var ans = ReadLine.question(val.question)
-        if (ans.toUpperCase() == val.ans.toUpperCase()){
-            score+=1
+  ]
+  let score = 0
+  const spinner = createSpinner('Checking Answer')
+  QnA.forEach((val) => {
+    var ans = ReadLine.question(val.question)
+    if (ans.toUpperCase() == val.ans.toUpperCase()) {
+      score += 1
 
-            setTimeout(() => {
-                spinner.success()
-              }, 1000)
-        }
-        else{
-            setTimeout(() => {
-                spinner.error()
-              }, 1000)
-        }
-    })
-
+      setTimeout(() => {
+        spinner.success()
+      }, 1000)
+    }
+    else {
+      setTimeout(() => {
+        spinner.error()
+      }, 1000)
+    }
+  })
+  setTimeout(() => {
+    console.log(`${username} your Score is ${score}/5`)
+  }, 1000)
 }
 
 Welcome()
